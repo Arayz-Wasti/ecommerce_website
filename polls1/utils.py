439261a4aa2ab82ec68ac2e395ebaa841ec5acea
Arayz-Wasti <arayzwasti111@gmail.com>
2024-07-29 11:54:51 +0500

    Added files via upload

--- polls1/utils.py
+++ polls1/utils.py
@@ -2,27 +2,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 def send_email_to_user():
     subject = 'Order Confirmation - iPhone 15'
-    message ="""
-Dear Momina Saeed,
-Thank you for shopping with Daraz. We are pleased to confirm your order for the iPhone 15.
-Order Details:
-Product: iPhone 15 (Black)
-Order Number: DZ-ORD-1202747382
-Payment Method: Cash on Delivery (COD)
-Recipient Details:
-Name: Momina Saeed
-Father's Name: Saeed Ahmad
-Location: Bahawalpur, Pakistan
-Mobile Number: +92 322 4485135
-Your order is being processed and will be shipped to you soon. You will receive a notification once the order is dispatched along with tracking details.
-
-Note: Please be aware that in the unlikely event you do not receive your order, the price of the order will be deducted from your bank account by tracing your account information. If necessary, we may also take legal action to ensure compliance with our payment policies.
-If you have any questions or need further assistance, please do not hesitate to contact our customer support team.
-Thank you for choosing Daraz. We hope you enjoy your new iPhone 15!
-Best regards,
-Customer Support Team
-Daraz
-"""
+    message ="your order has been successfully done"
     from_email = settings.EMAIL_HOST_USER
     recipient_list = ['naimatullahkhan0987@gmail.com']
     send_mail(subject, message, from_email, recipient_list)
